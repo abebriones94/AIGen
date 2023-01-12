bfc5a1a578215344bad58c80350a8295f1974350
Abe Briones <abebriones94@gmail.com>
2023-01-12 17:29:29 +0100

    Adding to github

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -9,7 +9,7 @@ const generateImage = async (req, res) => {
     const { prompt, size } = req.body;
 
     const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
-
+ 
        try {
         const response = await openai.createImage({
             prompt, 
@@ -35,7 +35,7 @@ const generateImage = async (req, res) => {
 
         res.status(400).json({
             success: false,
-            error: 'The image could not be generated'
+            error: 'The image could not be generated' 
        }); 
     }
 };
